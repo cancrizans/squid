fbb007b0af2d200855b0984ec113371a3b86031d
cancrizans <cancrizans.canon@gmail.com>
2020-02-19 09:18:05 +0100

    mmmm

--- squid.js
+++ squid.js
@@ -1,8 +1,19 @@
+toneSequences = {
+	'H': "˥",
+	'L': "˩",
+	'R': "˩˥",
+	'F': "˥˩",
+	'P': "˩˥˩",
+	'D': "˥˩˥",
+	'T': "˥˩˧˩",
+	'J': "˩˥˧˥"
+}
+
+
 function toIPA(text){
 
 
 	text = text.replace(/-/g,"")
-				.replace(/a/g,"ə.")
 				.replace(/mm/g,"mː")
 				.replace(/nny/g,"ɲː")
 				.replace(/nn/g,"nː")
@@ -21,6 +32,66 @@ function toIPA(text){
 				.replace(/tts/g,"tːs")
 				.replace(/y/g,"j");
 
-	return text;
+	words = text.split(" ");
+
+	outwords = [];
+
+	for (let w of words){
+		let m = w.match(/^((?:H|L|R|F|P|D|J|T)?)(.*)$/);
+		let tone = m[1];
+
+		if(tone == ""){
+			outwords.push(w);
+			continue;
+		}
+		
+		let syllables = m[2].trim().split("a").slice(0,-1);
+
+		let pitches = [...toneSequences[tone]];
+
+		if(pitches.length > syllables.length)
+		{
+			let a = pitches.pop();
+			let b = pitches.pop();
+			pitches.push(b+a);
+		}
+
+		if(pitches.length > syllables.length){
+			let a = pitches.shift();
+			let b = pitches.shift();
+			pitches.unshift(a+b);
+		}
+
+		if(syllables.length > pitches.length){
+
+			if(pitches.length == 2)
+			{
+				pitches.splice(1,0,'˧');
+			}
+		}
+
+		while(syllables.length > pitches.length){
+			pitches.splice(1,0,pitches[1]);
+		}
+
+
+		if(syllables.length != pitches.length)
+		{
+			outwords.push("ERROR");
+			continue;
+		}
+
+		let outsyllables = syllables.map( (s,i) => s+"ə"+pitches[i] );
+
+		outwords.push(outsyllables.join("."));
+
+	}
+
+
+
+
+
+
+	return outwords.join(" ");
 
 }
